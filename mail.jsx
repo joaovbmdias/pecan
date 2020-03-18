127270231a606cc9e20ba0b06b574a5b6e898bd0
Yeung Yiu Hung <hung@shoplineapp.com>
2020-03-18 10:13:28 +0800

    feat(mail): update interval

--- mail.jsx
+++ mail.jsx
@@ -1,5 +1,5 @@
 const command = "bash pecan/scripts/mail";
-const refreshFrequency = 60000; // ms
+const refreshFrequency = 1000; // ms
 
 const render = ({ output }) => <div class='screen'><div class='pecanmail'>{`📨 ${output}`}</div></div>;
 
