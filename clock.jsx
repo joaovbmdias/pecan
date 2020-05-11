65bdc9634f33a59d7457aafe36400f5e302cd9ec
Joao Dias <joaovbmdias@icloud.com>
2020-05-11 20:33:03 +0100

    Change to clock time format

--- clock.jsx
+++ clock.jsx
@@ -1,4 +1,4 @@
-const command = 'date +"%I:%M%p"';
+const command = 'date +"%H:%M"';
 const refreshFrequency = 60000; // ms
 
 const render = ({ output }) => <div class='screen'><div class='pecanclock'>→ {`${output}`}</div></div>;
